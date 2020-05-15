510923fdde4802d5e3dde85821fe912b472798f0
MichaelCabot <cabotmichael@gmail.com>
2020-05-15 06:02:38 -0400

    update submissions.php table styles
    add accordian (collapsible) tables

--- htdocs/output/submissions.php
+++ htdocs/output/submissions.php
@@ -20,12 +20,9 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-    NEEDS 3 THINGS:
-    Header showing Finalize button and total cheque amount
-*/
-
 require_once("../includes/mysql.config.php");
+
+setlocale(LC_MONETARY, 'en_US');
 
 //$timestamp = $_GET['timestamp']; // unix timestamp is unique identifier of submission
 //$timestamp = $conn->real_escape_string($timestamp); // secure data
@@ -34,7 +31,8 @@ require_once("../includes/mysql.config.php");
 $sql = "SELECT * FROM actions WHERE actionID = '1' ORDER BY timestamp ASC"; // get submission from actions table (oldest first)
 $result = $conn->query($sql);
 if ($result->num_rows < 1) { die("Error: No submissions found!"); } // error: submission not found
-
+$orderCount = 0;
+    
 while($row = $result->fetch_assoc()) {
     
     $timestamp = $row['timestamp'];
@@ -43,56 +41,72 @@ while($row = $result->fetch_assoc()) {
     $sqlItems = "";
     foreach($items as $item) { $sqlItems .= "item = " . $item . " OR "; } // build sql query string for selecting all items in submission
     $sqlItems = substr_replace($sqlItems ,"",-3);
-
+    
+    $sql = "SELECT SUM(lost_depracation_amount) AS total_value FROM contents WHERE $sqlItems"; // final select submission items query
+    $result2 = $conn->query($sql);
+    $row2 = $result2->fetch_assoc();
+    $total_value = number_format($row2['total_value'],2);
+    
     $sql = "SELECT * FROM contents WHERE $sqlItems ORDER BY lost_depracation_amount DESC"; // final select submission items query
     $result2 = $conn->query($sql);
-        
+
 ?>
 
-<div class="col-auto">
-    <div class="alert alert-secondary submission-header">
-        <i class="icon-info" style="font-size: 1.3em; margin-right: 5px;"></i> <?php echo $datetime; ?>
-    </div>
-    <table class="table table-striped table-responsive submission-table">
-        <colgroup>
-            <col id="item">
-            <col id="description">
-            <col id="qty">
-            <col id="unitprice">
-            <col id="moneytocollect">
-            <col id="needtospend">
-            <col id="status">
-        </colgroup>
-        <thead>
-            <tr>
-                <th scope="col">
-                    Item
-                </th>
-                <th scope="col">
-                    Description
-                </th>
-                <th class="priority-low" scope="col">
-                    Qty.
-                </th>
-                <th class="priority-low" scope="col">
-                    Price
-                </th>
-                <th scope="col">
-                    $$
-                </th>
-                <th scope="col">
-                    Spend
-                </th>
-                <th class="priority-low" scope="col">
-                    Status
-                </th>
-            </tr>
-        </thead>
-        <tbody>
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-auto">
+            <div data-toggle="collapse" data-target="#submission-<?php echo $timestamp; ?>" class="alert alert-secondary bg-gradient-light submission-header">
+                <div class="d-flex flex-wrap flex-row">
+                    <div class="flex-fill mr-auto">
+                        <i class="icon-info" style="font-size: 1.3em; margin-right: 5px;"></i> <span class="submission-datetime"><?php echo $datetime; ?></span>
+                    </div>
+                    <div class="flex-fill">
+                        <button onclick="alert('test')" id="finalizeItems" type="button" class="btn btn-large btn-success bg-gradient-success float-right">
+                            <span class="p-2">Finalize</span>
+                            <span class="badge badge-light" style="font-size: 0.9em;">$<?php echo $total_value; ?></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <table id="submission-<?php echo $timestamp; ?>" class="table table-striped table-responsive submission-table collapse">
+                <colgroup>
+                    <col id="item">
+                    <col id="description">
+                    <col id="qty">
+                    <col id="unitprice">
+                    <col id="moneytocollect">
+                    <col id="needtospend">
+                    <col id="status">
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            Item
+                        </th>
+                        <th scope="col">
+                            Description
+                        </th>
+                        <th class="priority-none" scope="col">
+                            Qty.
+                        </th>
+                        <th class="priority-none" scope="col">
+                            Price
+                        </th>
+                        <th scope="col">
+                            $$
+                        </th>
+                        <th class="priority-none" scope="col">
+                            Spend
+                        </th>
+                        <th scope="col">
+                            Status
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
 
 <?php
 
-    setlocale(LC_MONETARY, 'en_US');
     while($row2 = $result2->fetch_assoc()) {
         $itemid = $row2['item'];
         $unit_price = number_format($row2["unit_price"],2);
@@ -100,33 +114,33 @@ while($row = $result->fetch_assoc()) {
         $spend_amount = number_format($row2['spend_amount'],2);
     
 ?>
-            <tr>
-                <td class="align-middle">
-                    <?php echo $row2["item"]; ?>
-                </td>
-                <td class="align-middle"><?php echo $row2["description"]; ?></td>
-                <td class="align-middle priority-low"><?php echo $row2["quantity"]; ?></td>
-                <td class="align-middle priority-low">$<?php echo $unit_price; ?></td>
-                <td class="align-middle">$<?php echo $depracation; ?></td>
-                <td class="align-middle">$<?php echo $spend_amount; ?></td>
+                    <tr>
+                        <td class="align-middle">
+                            <?php echo $row2["item"]; ?>
+                        </td>
+                        <td class="align-middle"><?php echo $row2["description"]; ?></td>
+                        <td class="align-middle priority-none"><?php echo $row2["quantity"]; ?></td>
+                        <td class="align-middle priority-none">$<?php echo $unit_price; ?></td>
+                        <td class="align-middle">$<?php echo $depracation; ?></td>
+                        <td class="align-middle priority-none">$<?php echo $spend_amount; ?></td>
                 
 <?php
             
         switch ($row2['status']) {
             case 1: // status: not replaced
-                echo "<td class=\"align-middle status-notreplaced priority-low\">Not Replaced</td>";
+                echo "<td class=\"align-middle status-notreplaced\">Not Replaced</td>";
             break;
             case 2: // status: partial
-                echo "<td class=\"status-partial align-middle priority-low\">Partial</td>";
+                echo "<td class=\"status-partial align-middle\">Partial</td>";
             break;
             case 3: // status: replaced
-                echo "<td class=\"align-middle status-replaced priority-low\">Replaced</td>";
+                echo "<td class=\"align-middle status-replaced\">Replaced</td>";
             break;
             case 4: // status: receipt submitted
-                echo "<td class=\"align-middle status-submitted priority-low\">Submitted</td>";
+                echo "<td class=\"align-middle status-submitted\">Submitted</td>";
             break;
             case 5: // status: receipt finalized
-                echo "<td class=\"align-middle status-finalized priority-low\">Finalized</td>";
+                echo "<td class=\"align-middle status-finalized\">Finalized</td>";
             break;
         } // end switch
         echo "</tr>";
@@ -134,10 +148,11 @@ while($row = $result->fetch_assoc()) {
     
 ?>    
                 
-        </tbody>
-    </table>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
-
 <?php
         
 }
