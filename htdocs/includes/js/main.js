16119ed37a500b8a4002a0d30a6fd432a0d5c83b
MichaelCabot <cabotmichael@gmail.com>
2020-05-16 14:33:10 -0400

    fix syntax error

--- htdocs/includes/js/main.js
+++ htdocs/includes/js/main.js
@@ -44,11 +44,12 @@ $.urlParam = function(name) {
 
 function loadSiteData(data) {
     $.getJSON('output/submissions.php', { pending: "show" }, function(data) {
-    if (data.pending > 0) {
-        $("#submissionCount").html(data.pending);
-    } else {
-        $("#submissionCount").hide();
-    }
+        if (data.pending > 0) {
+            $("#submissionCount").html(data.pending);
+        } else {
+            $("#submissionCount").hide();
+        }
+    });
     var allItems = 
         data.partial.total +
         data.finalized.total +
